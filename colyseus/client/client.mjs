3ed792dcf89a33cd553372515e3582d638b0ca7a
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2022-12-09 15:36:01 +0200

    Some colyseus changes, for testing the Rust colyseus SDK

--- colyseus/client/client.mjs
+++ colyseus/client/client.mjs
@@ -2,8 +2,15 @@ import * as Colyseus from "colyseus.js";
 
 let client = new Colyseus.Client("ws://localhost:3000");
 
-client.joinOrCreate("foo").then(room => {
-    console.log(room.sessionId, "joined", room.name);
-}).catch(e => {
-    console.log("JOIN ERROR", e);
-});
+client
+    .joinOrCreate("foo")
+    .then(room => {
+        console.log(room.sessionId, "joined", room.name);
+        room.onMessage("*", (message_type, data) => {
+            console.log('message_type', message_type);
+            console.log('data', data);
+        });
+    })
+    .catch(e => {
+        console.log("JOIN ERROR", e);
+    });
--- colyseus/server/server.mjs
+++ colyseus/server/server.mjs
@@ -10,7 +10,13 @@ gameServer
         allowReconnectionTime: 120
     }).on("create", (room) => console.log("room created:", room.roomId))
     .on("dispose", (room) => console.log("room disposed:", room.roomId))
-    .on("join", (room, client) => console.log(client.id, "joined", room.roomId))
+    .on("join", (room, client) => {
+        // This works with the js client.
+        client.send("power_up", {kind: "ammo"});
+        // console.log(client.id, "joined", room.roomId);
+        // console.log('client', client);
+        // console.log('room', room);
+    })
     .on("leave", (room, client) => console.log(client.id, "left", room.roomId));
 
 gameServer.listen(port)
